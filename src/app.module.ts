b4c4532b90602dca2383d7da3e693759c6fdc92e
sebek78 <sebasporek@gmail.com>
2021-07-11 11:20:27 +0200

    Add passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
@@ -17,6 +18,7 @@ import { UsersModule } from './users/users.module';
         }),
         DatabaseModule,
         UsersModule,
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,13 +3,34 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from './user.entity';
 
+interface User {
+    userId: number;
+    username: string;
+    password: string;
+}
+
 @Injectable()
 export class UsersService {
+    private readonly users = [
+        {
+            userId: 1,
+            username: 'john',
+            password: 'changeme',
+        },
+        {
+            userId: 2,
+            username: 'maria',
+            password: 'guess',
+        },
+    ];
     constructor(
         @InjectRepository(Users)
         private usersRepository: Repository<Users>,
     ) {}
     findAll(): Promise<Users[]> {
         return this.usersRepository.find();
+    }
+    async findOne(username: string): Promise<User | undefined> {
+        return this.users.find((user) => user.username === username);
     }
 }
