e3296d500ac265ea0dd368322ddce836165670c7
sebek78 <sebasporek@gmail.com>
2021-10-10 10:56:49 +0200

    test build

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,15 +8,24 @@ import { Users } from 'src/users/users.entity';
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                type: 'postgres',
-                host: configService.get('DB_HOST'),
-                port: configService.get('DB_PORT'),
-                username: configService.get('DB_USERNAME'),
-                password: configService.get('DB_PASSWORD'),
-                database: configService.get('DB_DATABASE'),
-                entities: [Users],
-            }),
+            useFactory: (configService: ConfigService) => {
+                if (process.env.NODE_ENV !== 'production') {
+                    return {
+                        type: 'postgres',
+                        host: configService.get('DB_HOST'),
+                        port: configService.get('DB_PORT'),
+                        username: configService.get('DB_USERNAME'),
+                        password: configService.get('DB_PASSWORD'),
+                        database: configService.get('DB_DATABASE'),
+                        entities: [Users],
+                    };
+                } else {
+                    return {
+                        url: process.env.DATABASE_URL,
+                        ssl: { rejectUnauthorized: false },
+                    };
+                }
+            },
         }),
     ],
 })
